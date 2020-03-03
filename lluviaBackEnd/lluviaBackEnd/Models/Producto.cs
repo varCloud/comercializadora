e28d4b9dfe039aa9194c31fe284d10be52e1b0d0
erny <ernesto.aguilar@cmv.mx>
2020-03-03 10:47:01 -0600

    validaciones de model productos

--- lluviaBackEnd/lluviaBackEnd/Models/Producto.cs
+++ lluviaBackEnd/lluviaBackEnd/Models/Producto.cs
@@ -12,24 +12,35 @@ namespace lluviaBackEnd.Models
         public int idProducto { get; set; }
 
 
-        //[Display(Name = "nombre")]
-        //[Required(ErrorMessage = "Este campo no puede estar vacio")]
-        //public string  nombre { get; set; }
-
-        //[Display(Name = "descripcion")]
-        //[Required(ErrorMessage = "Este campo no puede estar vacio")]
+        [Display(Name = "descripcion")]
+        [Required(ErrorMessage = "Este campo no puede estar vacio")]
         public string  descripcion { get; set; }
 
-
+        [Display(Name = "idUnidadMedida")]
+        [Required(ErrorMessage = "Este campo no puede estar vacio")]
         public int idUnidadMedida { get; set; }
+
         public string DescripcionUnidadMedida { get; set; }
+
+        [Display(Name = "idLineaProducto")]
+        [Required(ErrorMessage = "Este campo no puede estar vacio")]
         public int idLineaProducto { get; set; }
+
         public string DescripcionLinea { get; set; }
+
+        [Display(Name = "cantidadUnidadMedida")]
+        [Required(ErrorMessage = "Este campo no puede estar vacio")]
         public int cantidadUnidadMedida { get; set; }
+
         public string codigoBarras { get; set; }
+
         public Boolean activo { get; set; }
+
+        [Display(Name = "articulo")]
+        [Required(ErrorMessage = "Este campo no puede estar vacio")]
         public string articulo { get; set; }
+
         public string[] idLineaProductoConsulta { get; set; }
-        //public string[] idUnidadMedidaConsulta { get; set; }
+        
     }
 }
