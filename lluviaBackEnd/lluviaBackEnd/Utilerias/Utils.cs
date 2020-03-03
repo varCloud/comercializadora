eb4d911c7cc819e535b71936001616cf2e6cc15d
vic <sapitopicador@gmail.com>
2020-03-03 10:30:19 -0600

    se agrega generar codigo de barras y qr

---
+++ lluviaBackEnd/lluviaBackEnd/Utilerias/Utils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using ZXing;
+
+namespace lluviaBackEnd.Utilerias
+{
+    public static class Utils
+    {
+        public static byte[] GenerarCodigoBarras(string cadena)
+        {
+            Image img = null;
+            using (var ms = new MemoryStream())
+            {
+                var writer = new ZXing.BarcodeWriter() { Format = BarcodeFormat.CODE_128 };
+                writer.Options.Height = 80;
+                writer.Options.Width = 280;
+                writer.Options.PureBarcode = false;
+                img = writer.Write(cadena);
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                return ms.ToArray();
+            }
+        }
+
+        public static byte[] GenerarQR(string cadena)
+        {
+            Image img = null;
+            using (var ms = new MemoryStream())
+            {
+                var writer = new BarcodeWriter() { Format = BarcodeFormat.QR_CODE };
+                writer.Options.Height = 200;
+                writer.Options.Width = 200;
+                img = writer.Write(cadena);
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                return ms.ToArray();
+            }
+        }
+
+
+    }
+}
