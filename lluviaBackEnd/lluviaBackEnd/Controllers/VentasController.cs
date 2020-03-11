5c7ff31acd29174e944f0560323d5f0d3dc53d16
erny <ernesto.aguilar@cmv.mx>
2020-03-11 16:27:28 -0600

    se inicia modulo de ventas

---
+++ lluviaBackEnd/lluviaBackEnd/Controllers/VentasController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using lluviaBackEnd.DAO;
+using lluviaBackEnd.Models;
+
+namespace lluviaBackEnd.Controllers
+{
+    public class VentasController : Controller
+    {
+        // GET: Ventas
+
+        public ActionResult Ventas()
+        {
+            Notificacion<List<Ventas>> notificacion = new Notificacion<List<Ventas>>();
+            notificacion = new VentasDAO().ObtenerVentas(new Models.Ventas() { idVenta = 0 });
+            //ViewBag.lstLineasDeProductos = new LineaProductoDAO().ObtenerLineaProductos();
+            //ViewBag.lstUnidadMedida = new LineaProductoDAO().ObtenerUnidadesMedidas();
+            ViewBag.lstVentas = notificacion.Modelo;
+            return View();
+        }
+
+
+
+        public ActionResult ObtenerVentas(Ventas ventas)
+        {
+            try
+            {
+                Notificacion<List<Ventas>> notificacion = new Notificacion<List<Ventas>>();
+                notificacion = new VentasDAO().ObtenerVentas(ventas);
+                Ventas p = new Ventas();
+                p = notificacion.Modelo[0];
+                return Json(p, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public ActionResult _ObtenerVentas(Ventas ventas)
+        {
+            try
+            {
+                Notificacion<List<Ventas>> notificacion = new Notificacion<List<Ventas>>();
+                notificacion = new VentasDAO().ObtenerVentas(ventas);
+                ViewBag.lstCompras = notificacion.Modelo;
+                return PartialView("_ObtenerVentas");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public ActionResult BuscarVentas(Ventas ventas)
+        {
+            try
+            {
+                Notificacion<List<Ventas>> notificacion = new Notificacion<List<Ventas>>();
+                notificacion = new VentasDAO().ObtenerVentas(ventas);
+
+                if (notificacion.Modelo != null)
+                {
+                    ViewBag.lstVentas = notificacion.Modelo;
+                    return PartialView("_BuscarVentas");
+                }
+                else
+                {
+                    ViewBag.titulo = "Mensaje: ";
+                    ViewBag.mensaje = notificacion.Mensaje;
+                    return PartialView("_SinResultados");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        public ActionResult GuardarVentas(List<Ventas> ventas)
+        {
+            try
+            {
+                Notificacion<Ventas> result = new Notificacion<Ventas>();
+                //result = new VentasDAO().GuardarVentas(ventas);
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ActualizarEstatusVentas(Ventas ventas)
+        {
+            try
+            {
+                Notificacion<Ventas> notificacion = new Notificacion<Ventas>();
+                notificacion = new VentasDAO().ActualizarEstatusVentas(ventas);
+                return Json(notificacion, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+
+    }
+
+
+}
---
+++ lluviaBackEnd/lluviaBackEnd/DAO/VentasDAO.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AccesoDatos;
+using lluviaBackEnd.Models;
+using lluviaBackEnd;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Web.Mvc;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace lluviaBackEnd.DAO
+{
+    public class VentasDAO
+    {
+        private IDbConnection db = null;
+
+        public Notificacion<List<Ventas>> ObtenerVentas(Ventas ventas)
+        {
+            Notificacion<List<Ventas>> notificacion = new Notificacion<List<Ventas>>();
+
+            try
+            {
+
+                using (db = new SqlConnection(ConfigurationManager.AppSettings["conexionString"].ToString()))
+                {
+                    var parameters = new DynamicParameters();
+
+                    parameters.Add("@idProducto", ventas.idVenta);
+
+                    var result = db.QueryMultiple("SP_CONSULTA_VENTAS", parameters, commandType: CommandType.StoredProcedure);
+                    var r1 = result.ReadFirst();
+                    if (r1.status == 200)
+                    {
+                        notificacion.Estatus = r1.status;
+                        notificacion.Mensaje = r1.mensaje;
+                        notificacion.Modelo = result.Read<Ventas>().ToList();
+                    }
+                    else
+                    {
+                        notificacion.Estatus = r1.status;
+                        notificacion.Mensaje = r1.mensaje;
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return notificacion;
+            
+        }
+        public Notificacion<Ventas> GuardarVentas(List<Ventas> venta)
+        {
+            Notificacion<Ventas> notificacion = new Notificacion<Ventas>();
+            try
+            {
+                using (db = new SqlConnection(ConfigurationManager.AppSettings["conexionString"].ToString()))
+                {
+                    var parameters = new DynamicParameters();
+
+                    //parameters.Add("@idProducto", venta.idVenta);
+
+                    //var result = db.QueryMultiple("SP_INSERTA_ACTUALIZA_VENTAS", parameters, commandType: CommandType.StoredProcedure);
+                    //var r1 = result.ReadFirst();
+                    //if (r1.status == 200)
+                    //{
+                    //    notificacion.Estatus = r1.status;
+                    //    notificacion.Mensaje = r1.mensaje;
+                    //    notificacion.Modelo = venta; 
+                    //}
+                    //else
+                    //{
+                    //    notificacion.Estatus = r1.status;
+                    //    notificacion.Mensaje = r1.mensaje;
+                    //    notificacion.Modelo = venta;
+                    //}
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return notificacion;
+        }
+
+
+        public Notificacion<Ventas> ActualizarEstatusVentas(Ventas ventas)
+        {
+            Notificacion<Ventas> notificacion = new Notificacion<Ventas>();
+            try
+            {
+                using (db = new SqlConnection(ConfigurationManager.AppSettings["conexionString"].ToString()))
+                {
+                    var parameters = new DynamicParameters();
+                    parameters.Add("@idProducto", ventas.idProducto);
+                    
+                    var result = db.QueryMultiple("SP_ACTUALIZA_STATUS_VENTAS", parameters, commandType: CommandType.StoredProcedure);
+                    var r1 = result.ReadFirst();
+                    if (r1.status == 200)
+                    {
+                        notificacion.Estatus = r1.status;
+                        notificacion.Mensaje = r1.mensaje;
+                        notificacion.Modelo = ventas; 
+                    }
+                    else
+                    {
+                        notificacion.Estatus = r1.status;
+                        notificacion.Mensaje = r1.mensaje;
+                        notificacion.Modelo = ventas;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return notificacion;
+        }
+
+    }
+}
