f252296bc79c73e321ece7ff81fba40bf010eff4
vic <sapitopicador@gmail.com>
2020-05-15 00:07:40 -0500

    se agregan los servicios para la bitacora

---
+++ lluviaBackEnd/lluviaBackEnd/WebServices/Modelos/Request/RequestObtenerPedidosInternos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lluviaBackEnd.WebServices.Modelos.Request
+{
+    public class RequestObtenerPedidosInternos
+    {
+        public int idPedidoInterno { get; set; }
+        public int idEstatusPedido { get; set; }
+        public int idAlmacenOrigen { get; set; }
+        public int idAlmacenDestino { get; set; }
+        public int idUsuario { get; set; }
+        public DateTime fechaInicio { get; set; }
+        public DateTime fechaFin { get; set; }
+               
+    }
+}
