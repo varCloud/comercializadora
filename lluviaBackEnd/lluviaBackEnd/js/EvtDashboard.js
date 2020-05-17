7b9dd95f3834fe29940afd7f1d90d6d00d7d0a1f
YessyAlmonte <yessyalmonte@gmail.com>
2020-05-16 19:32:32 -0500

    Se crea grafico de ventas por estacion

---
+++ lluviaBackEnd/lluviaBackEnd/js/EvtDashboard.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    $(".btnGraficoVentas").click(function (evt) {
+        evt.preventDefault();
+        $(".btnGraficoVentas").removeClass("active");
+        $(this).addClass("active");
+
+        $.ajax({
+            url: rootUrl("/DashBoard/_Grafico"),
+            data: { tipoGrafico: 1, tipoReporteGrafico: $(this).attr("tipoReporteGrafico") },
+            method: 'post',
+            dataType: 'html',
+            async: true,
+            beforeSend: function (xhr) {
+                ShowLoader();
+            },
+            success: function (data) {
+                OcultarLoader();
+                $("#GraficoVentasPorFecha").html(data);
+
+            },
+            error: function (xhr, status) {
+                OcultarLoader();
+            }
+        });
+    });
+
+ 
+
+});
