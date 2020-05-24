3dc0222d459c59ff35a1ffc5fb2840839c1e36a1
erny2024 <la3771783@gmail.com>
2020-05-24 02:27:12 -0500

    se agrega funcion con decimales y se agrega en vista de productos

--- lluviaBackEnd/lluviaBackEnd/js/Index.js
+++ lluviaBackEnd/lluviaBackEnd/js/Index.js
@@ -195,6 +195,15 @@ function esNumero(evt) {
     return true;
 }
 
+function esPrecio(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    //console.log(charCode);
+    if (charCode > 31 && (charCode < 48 || charCode > 57) && (charCode != 46) ) {
+            return false;
+    }
+    return true;
+}
 
 
 $(document).ready(function () {
