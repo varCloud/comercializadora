45d9b616188f2d106242cfd080c9298451b7d617
YessyAlmonte <yessyalmonte@gmail.com>
2020-07-28 00:21:56 -0500

    Se inicia con la vista de inventario fisico, se crea script para permisos de inventario fisico

---
+++ lluviaBackEnd/lluviaBackEnd/Controllers/InventarioFisicoController.cs
@@ -0,0 +1,21 @@
+﻿using lluviaBackEnd.Filters;
+using lluviaBackEnd.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lluviaBackEnd.Controllers
+{
+    [SessionTimeout]
+    public class InventarioFisicoController : Controller
+    {
+        // GET: InventarioFisico
+        public ActionResult InventarioFisico()
+        {
+            ViewBag.ListInventarioFisico = new List<InventarioFisico>();
+            return View();
+        }
+    }
+}
