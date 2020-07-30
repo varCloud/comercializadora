2ef9aa7e0761a1383d8b09bab932c040bf2533cd
YessyAlmonte <yessyalmonte@gmail.com>
2020-07-30 01:15:46 -0500

    Se agrega funcion que desactiva y activa el inventario fisico, actualizacion de nombre de inventario fisico (faltan detalles))

--- lluviaBackEnd/lluviaBackEnd/js/evtInventarioFisico.js
+++ lluviaBackEnd/lluviaBackEnd/js/evtInventarioFisico.js
@@ -94,3 +94,74 @@ function InitDataTableInventarioFisico() {
         tblInventarioFisico.table().container()
     );
 }
+
+function actualizarInventarioFisico(idTipoActualizacion, idInventarioFisico, ValorAnt, ValorNew) {
+
+    if ($.trim(ValorAnt) == $.trim(ValorNew)) {
+        return;
+    }
+
+    if (idTipoActualizacion == 1 && $.trim(ValorNew) == "") {
+        $("#txtNombre_" + idInventarioFisico).focus();
+        MuestraToast("error", "El nombre que desea actualizar no puede ir vacio");
+        return;
+    }
+
+
+
+    swal({
+        title: '',
+        text: idTipoActualizacion == 1 ? 'Estas seguro que deseas actualizar el nombre?' : (ValorNew == true ? 'Estas seguro que deseas activar este inventario fisico' : 'Estas seguro que deseas desactivar este inventario fisico?'),
+        icon: '',
+        buttons: ["Cancelar", "Aceptar"],
+        dangerMode: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {                
+
+                var dataToPost;
+
+                if (idTipoActualizacion == 1)
+                    dataToPost = JSON.stringify({ idInventarioFisico: idInventarioFisico, Nombre: $.trim(ValorNew) });
+                else
+                    dataToPost = JSON.stringify({ idInventarioFisico: idInventarioFisico, Activo: ValorNew });
+
+                $.ajax({
+                    url: rootUrl("/InventarioFisico/GuardarInventarioFisico"),
+                    data: dataToPost,
+                    method: 'POST',
+                    dataType: 'JSON',
+                    contentType: "application/json; charset=utf-8",
+                    async: true,
+                    beforeSend: function (xhr) {
+                        ShowLoader("Actualizando.");
+                    },
+                    success: function (data) {
+                        if (data.Estatus == 200) {
+                            MuestraToast('success', data.Mensaje);                           
+                            PintarTabla();
+                        } else {
+                            MuestraToast("error", data.Mensaje);
+                        }
+
+                        OcultarLoader();
+
+
+                    },
+                    error: function (xhr, status) {
+                        OcultarLoader();
+                        console.log('Hubo un problema al actualizar el inventario fisico, contactese con el administrador del sistema');
+                        console.log(xhr);
+                        console.log(status);
+                    }
+                });
+            } else {
+                console.log("cancelar");
+            }
+        });
+
+    console.log(idTipoActualizacion);
+    console.log(ValorAnt);
+    console.log(ValorNew);
+
+}
