bfedc1407860f8aca4a29919fa5a90bad8a3a61a
DESKTOP-G3TSDIR\Adriano <sapitopicador@gmail.com>
2020-09-28 23:41:18 -0500

    adecuaciones

--- Analisis/Pruebas/Script de pedido especial.sql
+++ Analisis/Pruebas/Script de pedido especial.sql
@@ -1,10 +1,13 @@
 
-DECLARE @idPedidoInterno int=null
+select * from PedidosInternos where idTipoPedidoInterno = 1 order by idPedidoInterno asc
+
+
+DECLARE @idPedidoInterno int=209
 
 select @idPedidoInterno=coalesce(@idPedidoInterno,(select max(idPedidoInterno) from pedidosInternos where idTipoPedidoInterno=2))
 
 select 'pedidosInternos',* from pedidosInternos where  idPedidoInterno=@idPedidoInterno
-select 'PedidosInternosLog',* from PedidosInternosLog where idPedidoInterno=@idPedidoInterno
+select 'PedidosInternosLog',P.*,EPI.descripcion from PedidosInternosLog  P join CatEstatusPedidoInterno EPI on P.IdEstatusPedidoInterno = EPI.IdEstatusPedidoInterno where P.idPedidoInterno=@idPedidoInterno
 select 'PedidosInternosDetalle',* from PedidosInternosDetalle where idPedidoInterno=@idPedidoInterno 
 select 'movimientosMercancia',* from MovimientosDeMercancia where idPedidoInterno=@idPedidoInterno
 order by idMovMercancia
@@ -23,7 +26,7 @@ order by idAlmacen,idUbicacion
 
 select 'InventarioDetalleLog',b.fechaAlta,u.idAlmacen,b.idUbicacion,b.idProducto,b.cantidad,cantidadActual,b.idTipoMovInventario,b.idPedidoInterno 
 from PedidosInternosDetalle a
-join InventarioDetalleLog b on a.idProducto=b.idProducto
+join InventarioDetalleLog b on a.idProducto=b.idProducto and A.idPedidoInterno = @idPedidoInterno
 join Ubicacion u on b.idUbicacion=u.idUbicacion
 where a.idPedidoInterno=@idPedidoInterno 
 order by b.idInventarioDetalleLOG
