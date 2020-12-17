f7bb07f660e547af904589fd3d1008dad61a4cc0
Victor Adrian Reyes <sapitopicador@gmail.com>
2020-12-17 16:06:46 -0600

    web service buscar por descripcion de producto

---
+++ lluviaBackEnd/lluviaBackEnd/WebServices/Modelos/Request/RequestObtenerProductoXDescripcion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lluviaBackEnd.WebServices.Modelos.Request
+{
+    public class RequestObtenerProductoXDescripcion
+    {
+        public string descripcion { get; set; }
+    }
+}
