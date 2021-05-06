e0e6a83c3487047cc22c81bec470d347a5b12ac1
Jessi <aoaj720209@cmv.mx>
2021-05-06 13:31:08 -0500

    Se realiza modificacion en el ticket de cierre de dia y la vista

---
+++ lluviaBackEnd/lluviaBackEnd/js/evtCierreCajas.js
@@ -0,0 +1,24 @@
+﻿
+function ImprimeTicketRetiro(idRetiro, tipoRetiro) {
+    $.ajax({
+        url: rootUrl("/Ventas/ImprimeTicketRetiro"),
+        data: { idRetiro: idRetiro, idCliente: idRetiro, tipoRetiro: tipoRetiro },
+        method: 'post',
+        dataType: 'html',
+        async: true,
+        beforeSend: function (xhr) {
+            ShowLoader();
+        },
+        success: function (data) {          
+            OcultarLoader();
+            MuestraToast('success', "Se envio el ticket a la impresora.");
+        },
+        error: function (xhr, status) {
+            OcultarLoader();
+            MuestraToast('error', "Ocurrio un error al enviar el ticket a la impresora.");
+            console.log(xhr);
+            console.log(status);
+            console.log(data);
+        }
+    });
+}
