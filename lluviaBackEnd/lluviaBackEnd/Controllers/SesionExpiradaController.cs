fb41974259ab584fac72d643973e381df2885e06
Victor Adrian Reyes <sapitopicador@gmail.com>
2021-05-05 23:50:37 -0500

    vista de sesion expirada
    vista de sesion expirada

---
+++ lluviaBackEnd/lluviaBackEnd/Controllers/SesionExpiradaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lluviaBackEnd.Controllers
+{
+    public class SesionExpiradaController : Controller
+    {
+        // GET: SesionExpirada
+        public ActionResult SesionExpirada()
+        {
+            return View();
+        }
+    }
+}
