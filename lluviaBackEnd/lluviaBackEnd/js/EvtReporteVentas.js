83aea20d5aaac8a9fcbd8437c8e895ddd4c708ff
YessyAlmonte <yessyalmonte@gmail.com>
2021-05-21 18:05:29 -0500

    Se agrega columna de monto total y forma de pago

--- lluviaBackEnd/lluviaBackEnd/js/EvtReporteVentas.js
+++ lluviaBackEnd/lluviaBackEnd/js/EvtReporteVentas.js
@@ -27,33 +27,14 @@ function InitDataTableVentas() {
     if ($("#tablaRepVentas").length > 0) {
         new $.fn.dataTable.Buttons(tablaVentas, {
             buttons: [
-                {
-                    extend: 'pdfHtml5',
-                    text: '<i class="fas fa-file-pdf" style="font-size:20px;"></i>',
-                    className: '',
-                    titleAttr: 'Exportar a PDF',
-                    title: "Reporte Ventas",
-                    customize: function (doc) {
-                        doc.defaultStyle.fontSize = 8;
-                        doc.styles.tableHeader.fontSize = 10;
-                        doc.defaultStyle.alignment = 'center';
-                        //doc.content[1].table.widths = ['20%', '20%', '10%', '10%', '10%', '10%', '15%', '10%'];
-                        doc.pageMargins = [30, 85, 20, 30];
-                        doc.content.splice(0, 1);
-                        doc['header'] = SetHeaderPDF("Reporte Ventas");
-                        doc['footer'] = (function (page, pages) { return setFooterPDF(page, pages) });
-                    },
-                    exportOptions: {
-                        columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-                    },
-                },
+            
                 {
                     extend: 'excel',
                     text: '<i class="fas fa-file-excel" style="font-size:20px;"></i>',
                     className: '',
                     titleAttr: 'Exportar a Excel',
                     exportOptions: {
-                        columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+                        columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,13,14]
                     },
                 },
             ],
