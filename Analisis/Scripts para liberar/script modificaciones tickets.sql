36db022002dade5ca5d3a7981ee005723ecda478
erny2024 <la3771783@gmail.com>
2021-06-09 00:32:52 -0500

    se inicia con la modificacion de tickets

---
+++ Analisis/Scripts para liberar/script modificaciones tickets.sql
@@ -0,0 +1,16 @@
+use DB_A57E86_lluviadesarrollo
+go
+
+
+alter table complementos add montoPagadoAgregarProductos money
+go
+
+alter table complementos add montoAgregarProductos money
+go
+
+
+
+
+--select * from Ventas
+--select * from Complementos
+--select * from ComplementosDetalle
