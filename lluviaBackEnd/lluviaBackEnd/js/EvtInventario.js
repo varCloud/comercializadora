0049da8740c79f4fa1dd4eb79b5dfe8690122f7f
YessyAlmonte <yessyalmonte@gmail.com>
2021-06-18 17:37:16 -0500

    Se limita que no se permita seleccionar otra fecha mayor a la de hoy

--- lluviaBackEnd/lluviaBackEnd/js/EvtInventario.js
+++ lluviaBackEnd/lluviaBackEnd/js/EvtInventario.js
@@ -66,6 +66,102 @@ $(document).ready(function () {
 
     InitDataTableInventario();
     InitSelect2();
+
+
+    var table;
+    var iframe;
+    var tablaInventario;
+
+
+    //busqueda
+    function onBeginSubmitProductos() {
+        ShowLoader("Buscando...");
+    }
+    function onSuccessResultProductos(data) {
+        $('#rowProductos').html(data);
+        tablaInventario.destroy();
+        InitDataTableInventario();
+        OcultarLoader();
+    }
+    function onFailureResultProductos() {
+        OcultarLoader();
+    }
+
+    function InitDataTableInventario() {
+        var NombreTabla = "tablaRepProductos";
+        tablaInventario = initDataTable(NombreTabla);
+        if ($("#tablaRepProductos").length > 0) {
+            new $.fn.dataTable.Buttons(tablaInventario, {
+                buttons: [
+                    {
+                        extend: 'pdfHtml5',
+                        text: '<i class="fas fa-file-pdf" style="font-size:20px;"></i>',
+                        className: '',
+                        titleAttr: 'Exportar a PDF',
+                        title: "Inventario",
+                        customize: function (doc) {
+                            doc.defaultStyle.fontSize = 8;
+                            doc.styles.tableHeader.fontSize = 10;
+                            doc.defaultStyle.alignment = 'center';
+                            //doc.content[1].table.widths = ['10%', '20%', '20%', '20%', '20%', '10%'];
+                            doc.pageMargins = [30, 85, 20, 30];
+                            doc.content.splice(0, 1);
+                            doc['header'] = SetHeaderPDF("Inventario");
+                            doc['footer'] = (function (page, pages) { return setFooterPDF(page, pages) });
+                        },
+                        exportOptions: {
+                            columns: [0, 1, 2, 3, 4, 5, 6]
+                        },
+                    },
+                    {
+                        extend: 'excel',
+                        text: '<i class="fas fa-file-excel" style="font-size:20px;"></i>',
+                        className: '',
+                        titleAttr: 'Exportar a Excel',
+                        exportOptions: {
+                            columns: [0, 1, 2, 3, 4, 5, 6]
+                        },
+                    },
+                ],
+
+            });
+
+            tablaInventario.buttons(0, null).container().prependTo(
+                tablaInventario.table().container()
+            );
+        }
+    }
+
+    $(document).ready(function () {
+
+        InitDataTableInventario();
+        InitSelect2();
+
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1; //January is 0 so need to add 1 to make it 1!
+        var yyyy = today.getFullYear();
+        if (dd < 10) {
+            dd = '0' + dd
+        }
+        if (mm < 10) {
+            mm = '0' + mm
+        }
+
+        today = yyyy + '-' + mm + '-' + dd;
+        $("#fechaAlta").attr("max", today);
+        //document.getElementById("datefield").setAttribute("min", today);
+
+        //InitRangePicker('rangeInventario', 'fechaIni', 'fechaFin');
+        ////$('#idLineaProductoBusqueda').val('0');
+        //$('#fechaIni').val($('#rangeInventario').data('daterangepicker').startDate.format('YYYY-MM-DD'));
+        //$('#fechaFin').val($('#rangeInventario').data('daterangepicker').startDate.format('YYYY-MM-DD'));
+
+    });
+
+
+    //document.getElementById("datefield").setAttribute("min", today);
+
     //InitRangePicker('rangeInventario', 'fechaIni', 'fechaFin');
     ////$('#idLineaProductoBusqueda').val('0');
     //$('#fechaIni').val($('#rangeInventario').data('daterangepicker').startDate.format('YYYY-MM-DD'));
