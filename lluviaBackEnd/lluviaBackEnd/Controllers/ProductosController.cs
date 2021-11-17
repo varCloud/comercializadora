925e083d5bda6c38793a2a4614e51a67c2a047f8
varCloud <sapitopicador@gmail.com>
2021-11-17 12:25:04 -0600

    se agrega el MaxJsonLength en ObtenerProductosPorAlmacen
    se agrega el MaxJsonLength en ObtenerProductosPorAlmacen

--- lluviaBackEnd/lluviaBackEnd/Controllers/ProductosController.cs
+++ lluviaBackEnd/lluviaBackEnd/Controllers/ProductosController.cs
@@ -85,7 +85,9 @@ namespace lluviaBackEnd.Controllers
                 producto.idUsuario = UsuarioActual.idUsuario;
                 Notificacion<List<Producto>> notificacion = new Notificacion<List<Producto>>();
                 notificacion = new ProductosDAO().ObtenerProductosPorUsuario(producto);
-                return Json(notificacion, JsonRequestBehavior.AllowGet);
+                var json = Json(notificacion, JsonRequestBehavior.AllowGet);
+                json.MaxJsonLength = 500000000;
+                return json;
             }
             catch (Exception ex)
             {
@@ -103,7 +105,6 @@ namespace lluviaBackEnd.Controllers
                 notificacion = new ProductosDAO().ObtenerProductosPorAlmacen(idAlmacen);
                 var json = Json(notificacion, JsonRequestBehavior.AllowGet);
                 json.MaxJsonLength = 500000000;
-
                 return json;
             }
             catch (Exception ex)
