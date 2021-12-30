c76c4406d0cd836448ae4484c4e6e624c949e792
CMV\auhl373453 <ernesto.aguilar@cmv.mx>
2021-12-30 11:03:03 -0600

    se agrega ubicacion de devolucion para pedidos especiales, y forma de pago a credito en catalogo de forma de pago

--- Analisis/Scripts pedidos especiales/1.- crea la ubicacion de resguardo en los catalogos de raq, piso y pasillo.sql
+++ Analisis/Scripts pedidos especiales/1.- crea la ubicacion de resguardo en los catalogos de raq, piso y pasillo.sql
@@ -26,3 +26,33 @@ insert into CatPasillo (descripcion) values ('Resguardo')
 select @indice = max(idPasillo) from CatPasillo where idPasillo < 1000
 DBCC CHECKIDENT ('CatPasillo', RESEED, @indice)
 
+
+-- ubicacion para regresar productos de un pedido especial
+
+DBCC CHECKIDENT ('CatPiso', RESEED, 1000)
+insert into [CatPiso] (descripcion) values ('Devolución Pedido Especial')
+select @indice = max(idPiso) from [CatPiso] where idPiso < 1000
+DBCC CHECKIDENT ('CatPiso', RESEED, @indice)
+
+DBCC CHECKIDENT ('CatRaq', RESEED, 1000)
+insert into CatRaq (descripcion) values ('Devolución Pedido Especial')
+select @indice = max(idRaq) from CatRaq where idRaq < 1000
+DBCC CHECKIDENT ('CatRaq', RESEED, @indice)
+
+DBCC CHECKIDENT ('CatPasillo', RESEED, 1000)
+insert into CatPasillo (descripcion) values ('Devolución Pedido Especial')
+select @indice = max(idPasillo) from CatPasillo where idPasillo < 1000
+DBCC CHECKIDENT ('CatPasillo', RESEED, @indice)
+
+select * from Ubicacion where idPasillo = 1001 and idRaq =1001 and idPiso =1001
+
+
+-- se inserta forma de pago a credito para pedidos especiales
+insert into [FactCatFormaPago] (id, formaPago, descripcion) values (100, '100','Crédito')
+
+/*
+select * from FactCatFormaPago where id = 100
+select * from [dbo].[CatPasillo] where idPasillo = 1001
+select * from [dbo].[CatRaq] where idRaq = 1001
+select * from [dbo].[CatPiso] where idPiso = 1001
+*/
