213de8a04dc7cb2bba0d58f332f51ce54fcc1166
erny2024 <la3771783@gmail.com>
2022-02-19 23:41:56 -0600

    se agrega estatus de rechazo parcial por administrador

--- Analisis/Scripts Incidencias Febrero 2022/Alter table PedidosEspecialesAbonoClientes.sql
+++ Analisis/Scripts Incidencias Febrero 2022/Alter table PedidosEspecialesAbonoClientes.sql
@@ -2,3 +2,8 @@ ALTER TABLE PedidosEspecialesAbonoClientes add saldoInicial money default 0,sald
 
 
 
+if not exists (select 1 from CatEstatusPedidoEspecialDetalle where descripcion like 'Rechazo Parcial por el Administrador')
+begin
+	insert into CatEstatusPedidoEspecialDetalle(descripcion) values ('Rechazo Parcial por el Administrador')
+end
+
