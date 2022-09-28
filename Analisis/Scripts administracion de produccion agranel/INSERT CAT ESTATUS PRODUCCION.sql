a1ff3aa855a47caa96265f861f94b53aad96da06
Adrian Reyes <vreyes@grainchain.io>
2022-09-27 22:01:02 -0500

    se termina el desarrollo de proceso de produccion agranel
    se termina el desarrollo de proceso de produccion agranel

---
+++ Analisis/Scripts administracion de produccion agranel/INSERT CAT ESTATUS PRODUCCION.sql
@@ -0,0 +1,11 @@
+
+GO
+	INSERT INTO [dbo].[CatEstatusProcesoAgranel] ([descripcion]) VALUES ('Pendiende de procesar')
+	INSERT INTO [dbo].[CatEstatusProcesoAgranel] ([descripcion]) VALUES ('Pendiende de procesar')
+	INSERT INTO [dbo].[CatEstatusProcesoAgranel] ([descripcion]) VALUES ('Procesado Correctamente')
+	INSERT INTO [dbo].[CatEstatusProcesoAgranel] ([descripcion]) VALUES ('Procesado con Rechazo Total')
+	INSERT INTO [dbo].[CatEstatusProcesoAgranel] ([descripcion]) VALUES ('Procesado con Rechazo Parcial')
+GO
+
+
+	select * from [CatEstatusProcesoAgranel]
